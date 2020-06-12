6952819f1265ab49261d1473c5fd849e6fb6ec89
yuhwanwoo <6514687@naver.com>
2020-06-12 10:28:44 +0900

    django form

---
+++ pages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def loop(request):
+    nums=[]
+    for data in range(10):
+        nums.append(data)
+    context={
+        'nums':nums
+    }
+    return render(request,'loop.html',context)
